47dd8b9b50dd1923d82bc7decb1b20385a3bc99f
azzamalzid <124938717+azzamalzid@users.noreply.github.com>
2024-06-13 03:38:16 +0300

    23

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <meta name="description" content="" />
         <meta name="author" content="" />
-        <title>Freelancer111 - Start Bootstrap Theme</title>
+        <title>Freelancer111 Azzam  - Start Bootstrap Theme</title>
         <!-- Favicon-->
         <link rel="icon" type="image/x-icon" href="{{ url('assets/favicon.ico') }}" />
         <!-- Font Awesome icons (free version)-->
